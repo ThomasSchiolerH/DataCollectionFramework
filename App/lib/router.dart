cccc7cd72944a4b5b0ad6077bdadd5abcc90d7bd
ThomasSchiolerH <thomas@schioeler.com>
2024-01-03 14:37:41 +0100

    authentication screen UI

--- App/lib/router.dart
+++ App/lib/router.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:mental_health_app/features/auth/screens/auth_screen.dart';
 import 'package:mental_health_app/main.dart';
 import 'package:mental_health_app/testScreen.dart';
 
 
 Route<dynamic> generateRoute(RouteSettings routeSettings) {
   switch(routeSettings.name) {
-    case testScreen.routeName:
+    case AuthScreen.routeName:
       return MaterialPageRoute(
         settings: routeSettings,
-        builder: (_) => const testScreen(),
+        builder: (_) => const AuthScreen(),
       );
     default:
       return MaterialPageRoute(
