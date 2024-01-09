ae89fa35d47c48c4bce8c696b69d01e186b83de3
ThomasSchiolerH <thomas@schioeler.com>
2024-01-09 17:45:14 +0100

    backend setting up for auth

---
+++ App/lib/features/auth/services/auth_services.dart
@@ -0,0 +1,23 @@
+import 'package:mental_health_app/models/user.dart';
+import 'package:mental_health_app/constants/global_variables.dart';
+import 'package:http/http.dart' as http;
+
+class AuthServices {
+  void signUpUser({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      User user =
+          User(id: '', name: name, email: email, password: password, type: '');
+      
+      http.Response res = await http.post(Uri.parse("$uri/api/signup"),
+      body: user.toJson(),
+      headers: <String, String>{
+        'Content-Type': 'applications/json; charset=UTF-8',
+      },
+      );
+    } catch (e) {}
+  }
+}
---
+++ server/index.js
@@ -0,0 +1,29 @@
+// Imports from packages
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Imports from files
+const authRouter = require("./routes/auth");
+
+// INIT
+const PORT = 3001;
+const app = express();
+const DB = "mongodb+srv://thomas:u5jCaLhzbXFHkGUm@bachelorframework.dhooq54.mongodb.net/?retryWrites=true&w=majority"
+
+// Middleware
+app.use(express.json());
+app.use(authRouter);
+
+// Connections
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection to database successful");
+  })
+  .catch((e) => {
+    console.log(`Error connecting to database ${e}`);
+  });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`connected at port ${PORT}`);
+});
