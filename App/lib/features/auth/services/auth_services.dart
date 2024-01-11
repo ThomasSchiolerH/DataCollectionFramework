4e9844150f86d6a3c868242af62a844fe5f774e1
ThomasSchiolerH <thomas@schioeler.com>
2024-01-11 16:17:17 +0100

    providers, token added, blank home screen

--- App/lib/features/auth/services/auth_services.dart
+++ App/lib/features/auth/services/auth_services.dart
@@ -9,14 +9,21 @@ class AuthServices {
     required String password,
   }) async {
     try {
-      User user =
-          User(id: '', name: name, email: email, password: password, type: '');
-      
-      http.Response res = await http.post(Uri.parse("$uri/api/signup"),
-      body: user.toJson(),
-      headers: <String, String>{
-        'Content-Type': 'applications/json; charset=UTF-8',
-      },
+      User user = User(
+        id: '',
+        name: name,
+        email: email,
+        password: password,
+        type: '',
+        token: '',
+      );
+
+      http.Response res = await http.post(
+        Uri.parse("$uri/api/signup"),
+        body: user.toJson(),
+        headers: <String, String>{
+          'Content-Type': 'applications/json; charset=UTF-8',
+        },
       );
     } catch (e) {}
   }
