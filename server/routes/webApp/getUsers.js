5f14d821bfba25355aafb072fd52ab21caa977c0
ThomasSchiolerH <thomas@schioeler.com>
2024-01-21 14:13:18 +0100

    setup for getUser API

---
+++ server/routes/webApp/getUsers.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const User = require("../../models/user");
+const getUserRouter = express.Router();
+
+// Get users route
+getUserRouter.get("/api/getUser", async (req, res) => {
+    try{
+
+    } catch (e) {
+        
+    }
+});
+// Make public
+module.exports = getUserRouter;
