3ced139d159025f425a55cb332c354f5072f712f
AndreasBardram <105062795+AndreasBardram@users.noreply.github.com>
2024-01-31 14:04:26 +0100

    calendarUpdate

--- App/lib/features/home/screens/calendar_screen.dart
+++ App/lib/features/home/screens/calendar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
 
 class CalendarScreen extends StatelessWidget {
   static const String routeName = "/calendar";
@@ -11,8 +12,29 @@ class CalendarScreen extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('This is the Calendar Screen'),
-          // Remove the obsolete button
+          Text('Calendar Screen'),
+          HeatMapCalendar(
+            defaultColor: Colors.white,
+            flexible: true,
+            colorMode: ColorMode.color,
+            datasets: {
+              DateTime(2024, 1, 6): 1,
+              DateTime(2024, 1, 7): 5,
+              DateTime(2024, 1, 8): 2,
+              DateTime(2024, 1, 9): 3,
+              DateTime(2024, 1, 13): 4,
+            },
+            colorsets: const {
+              1: Color.fromARGB(255, 218, 140, 134),
+              2: Color.fromARGB(255, 227, 110, 110),
+              3: Color.fromARGB(255, 231, 76, 76),
+              4: Color.fromARGB(255, 234, 52, 52),
+              5: Color.fromARGB(255, 239, 7, 7),
+            },
+            onClick: (value) {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(value.toString())));
+            },
+          ),
         ],
       ),
     );
