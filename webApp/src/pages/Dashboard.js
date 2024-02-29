6b35e85b56668e7a2e6e29ca868a2edbf0a42bca
ThomasSchiolerH <thomas@schioeler.com>
2024-02-29 18:44:36 +0100

    dashboard cards created

--- webApp/src/pages/Dashboard.js
+++ webApp/src/pages/Dashboard.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import '../styles/UserTable.css';
+import '../styles/Dashboard.css';
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1 class="admin-dashboard-title-text">Admin Dashboard</h1>
-      {/* Dashboard content */}
+    <div className="dashboard">
+      <h1 className="admin-dashboard-title-text">Admin Dashboard</h1>
+      <div className="dashboard-grid">
+        <div className="card">
+          <div className="card-title">Active Users</div>
+          <div className="card-content">Content goes here...</div>
+        </div>
+        <div className="card large">
+          <div className="card-title">Users</div>
+          <div className="card-content">Larger content goes here...</div>
+        </div>
+        <div className="card">
+          <div className="card-title">Health Data</div>
+          <div className="card-content">Content goes here...</div>
+        </div>
+        <div className="card large">
+          <div className="card-title">Large Chart 2</div>
+          <div className="card-content">Larger content goes here...</div>
+        </div>
+        {/* Add more cards as needed */}
+      </div>
     </div>
   );
 };
