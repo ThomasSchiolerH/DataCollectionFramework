2370ee02b50672592c613d48a9b08cf101229aab
ThomasSchiolerH <thomas@schioeler.com>
2024-02-29 14:52:25 +0100

    Grouping data types in details page

--- webApp/src/pages/UserDetails.js
+++ webApp/src/pages/UserDetails.js
@@ -2,17 +2,31 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getUserHealthData } from '../services/getUserServices';
 
+const groupHealthDataByType = (healthData) => {
+  return healthData.reduce((acc, data) => {
+    // If the accumulator already has the type, push to it, else create a new array
+    if (!acc[data.type]) {
+      acc[data.type] = [];
+    }
+    acc[data.type].push(data);
+    return acc;
+  }, {});
+};
+
+
 const UserDetails = () => {
   const { userId } = useParams();
+  const [groupedHealthData, setGroupedHealthData] = useState({});
   const [healthData, setHealthData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
 
-  useEffect(() => {
+    useEffect(() => {
     const fetchHealthData = async () => {
       try {
         const data = await getUserHealthData(userId);
-        setHealthData(data);
+        const groupedData = groupHealthDataByType(data);
+        setGroupedHealthData(groupedData);
         setLoading(false);
       } catch (err) {
         console.error('Failed to fetch health data:', err);
@@ -22,7 +36,7 @@ const UserDetails = () => {
     };
 
     fetchHealthData();
-  }, [userId]);
+  }, [userId]); // Depend on userId so it refetches if that changes
 
   if (loading) {
     return <div>Loading...</div>;
@@ -36,14 +50,18 @@ const UserDetails = () => {
     <div>
       <h1>User Details Page</h1>
       <p>User ID: {userId}</p>
-      <h2>Health Data</h2>
-      <ul>
-        {healthData.map((data, index) => (
-          <li key={index}>
-            Type: {data.type}, Value: {data.value} {data.unit}, Date: {new Date(data.date).toLocaleDateString()}
-          </li>
-        ))}
-      </ul>
+      {Object.keys(groupedHealthData).map((type) => (
+        <div key={type}>
+          <h2>{type.charAt(0).toUpperCase() + type.slice(1)} Data</h2>
+          <ul>
+            {groupedHealthData[type].map((data, index) => (
+              <li key={index}>
+                Type: {data.type}, Value: {data.value} {data.unit}, Date: {new Date(data.date).toLocaleDateString()}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
