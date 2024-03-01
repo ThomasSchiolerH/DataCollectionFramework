9ad94ae639b0fd890f6edb7dbf2a96b772588d3b
ThomasSchiolerH <thomas@schioeler.com>
2024-03-01 13:11:05 +0100

    added active users on dashboard

--- webApp/src/pages/Dashboard.js
+++ webApp/src/pages/Dashboard.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getUserCount } from '../services/getUserServices';
 import '../styles/Dashboard.css';
 
 const Dashboard = () => {
+  const [activeUsers, setActiveUsers] = useState(0);
+
+  useEffect(() => {
+    const fetchUserCount = async () => {
+      const count = await getUserCount();
+      setActiveUsers(count);
+    };
+
+    fetchUserCount();
+  }, []);
+
   return (
     <div className="dashboard">
       <h1 className="admin-dashboard-title-text">Admin Dashboard</h1>
       <div className="dashboard-grid">
         <div className="card">
           <div className="card-title">Active Users</div>
-          <div className="card-content">Content goes here...</div>
+          <div className="card-content">{activeUsers}</div>
         </div>
         <div className="card large">
           <div className="card-title">Users</div>
