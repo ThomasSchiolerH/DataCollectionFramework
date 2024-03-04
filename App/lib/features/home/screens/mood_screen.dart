451b774129849a6ec53cad7269746e01a7c0d7e2
AndreasBardram <105062795+AndreasBardram@users.noreply.github.com>
2024-03-04 21:06:52 +0100

    loading feature after login to mask moodscreen

--- App/lib/features/home/screens/mood_screen.dart
+++ App/lib/features/home/screens/mood_screen.dart
@@ -20,6 +20,17 @@ class MoodScreen extends StatefulWidget {
 class _MoodScreenState extends State<MoodScreen> {
   String _currentDate = '';
   String _currentDateFormatted = '';
+  bool _isLoading = true; // New loading state
+
+  // Define a list of prefixes for each mood description
+  List<String> moodPrefixes = [
+    "terrible: ", // Prefix for mood 1
+    "bad: ", // Prefix for mood 2
+    "okay: ", // Prefix for mood 3
+    "good: ", // Prefix for mood 4
+    "very good: ", // Prefix for mood 5
+    "excellent: " // Prefix for mood 6
+  ];
 
   @override
   void initState() {
@@ -30,10 +41,9 @@ class _MoodScreenState extends State<MoodScreen> {
     _checkUserInputForToday();
   }
 
-void _checkUserInputForToday() async {
-  // Assuming you have a way to get the userId dynamically as needed
+  void _checkUserInputForToday() async {
   final userProvider = Provider.of<UserProvider>(context, listen: false);
-  final String userId = userProvider.user.id; // Use the dynamic user ID from UserProvider
+  final String userId = userProvider.user.id;
   final String currentDateFormatted = DateFormat('yyyy-MM-dd').format(DateTime.now());
 
   try {
@@ -41,7 +51,7 @@ void _checkUserInputForToday() async {
       Uri.parse('$uri/api/users/$userId/hasInputForDate?date=$currentDateFormatted'),
       headers: <String, String>{
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ${userProvider.user.token}', // Use the token from UserProvider
+        'Authorization': 'Bearer ${userProvider.user.token}',
       },
     );
 
@@ -50,8 +60,14 @@ void _checkUserInputForToday() async {
       final bool hasInput = data['hasInput'];
 
       if (hasInput) {
-        WidgetsBinding.instance.addPostFrameCallback((_) {
+        // Delay the navigation to HomeScreen by a short duration
+        Future.delayed(Duration(milliseconds: 500), () {
           Navigator.pushReplacementNamed(context, HomeScreen.routeName);
+        });
+      } else {
+        // If user doesn't have input, set loading to false to show MoodScreen
+        setState(() {
+          _isLoading = false;
         });
       }
     } else {
@@ -65,86 +81,86 @@ void _checkUserInputForToday() async {
 
   @override
   Widget build(BuildContext context) {
-    // Define a list of prefixes for each mood description
-    List<String> moodPrefixes = [
-      "terrible: ", // Prefix for mood 1
-      "bad: ", // Prefix for mood 2
-      "okay: ", // Prefix for mood 3
-      "good: ", // Prefix for mood 4
-      "very good: ", // Prefix for mood 5
-      "excellent: " // Prefix for mood 6
-    ];
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.close, color: Colors.black),
-          onPressed: () => Navigator.pushReplacementNamed(context, '/home'),
+    if (_isLoading) {
+      // Show loader while checking user input
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
         ),
-      ),
-      body: Container(
-        color: Colors.white,
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Align(
-          alignment: Alignment.topCenter,
-          child: Container(
-            margin: const EdgeInsets.only(top: 20), // Top margin of 20px
-            child: Column(
-              crossAxisAlignment:
-                  CrossAxisAlignment.start, // Align items to the left
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(bottom: 20.0),
-                  child: Text(
-                    'How are you feeling today?',
-                    style: TextStyle(
-                      fontSize: 22, // Increased font size
-                      fontWeight: FontWeight.bold, // Bold font weight
-                      color: Colors.black, // Adjusted text color
+      );
+    } else {
+      // Show MoodScreen only when loading is false
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          leading: IconButton(
+            icon: const Icon(Icons.close, color: Colors.black),
+            onPressed: () => Navigator.pushReplacementNamed(context, '/home'),
+          ),
+        ),
+        body: Container(
+          color: Colors.white,
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              margin: const EdgeInsets.only(top: 20), // Top margin of 20px
+              child: Column(
+                crossAxisAlignment:
+                CrossAxisAlignment.start, // Align items to the left
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 20.0),
+                    child: Text(
+                      'How are you feeling today?',
+                      style: TextStyle(
+                        fontSize: 22, // Increased font size
+                        fontWeight: FontWeight.bold, // Bold font weight
+                        color: Colors.black, // Adjusted text color
+                      ),
                     ),
                   ),
-                ),
-                Text(
-                  'Select a mood for $_currentDate:',
-                  style: const TextStyle(fontSize: 18),
-                ),
-                const SizedBox(height: 20),
-                ...List.generate(6, (index) {
-                  Color buttonColor = _getColorForMood(index + 1);
-                  return Padding(
-                    padding: const EdgeInsets.only(
-                        bottom: 8.0), // Space between buttons
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white, backgroundColor: buttonColor, // Text color
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 10), // Button padding
-                      ),
-                      onPressed: () =>
-                          _selectMoodAndNavigate(context, index + 1),
-                      child: Row(
-                        mainAxisSize: MainAxisSize
-                            .min, // To keep the Row content as compact as possible
-                        children: [
-                          Text(
-                            "${moodPrefixes[index]}${index + 1}", // Use the prefix for each button
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold, // Make the text bold
+                  Text(
+                    'Select a mood for $_currentDate:',
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  const SizedBox(height: 20),
+                  ...List.generate(6, (index) {
+                    Color buttonColor = _getColorForMood(index + 1);
+                    return Padding(
+                      padding: const EdgeInsets.only(
+                          bottom: 8.0), // Space between buttons
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white, backgroundColor: buttonColor, // Text color
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10), // Button padding
+                        ),
+                        onPressed: () =>
+                            _selectMoodAndNavigate(context, index + 1),
+                        child: Row(
+                          mainAxisSize: MainAxisSize
+                              .min, // To keep the Row content as compact as possible
+                          children: [
+                            Text(
+                              "${moodPrefixes[index]}${index + 1}", // Use the prefix for each button
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold, // Make the text bold
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                  );
-                }),
-              ],
+                    );
+                  }),
+                ],
+              ),
             ),
           ),
         ),
-      ),
-    );
+      );
+    }
   }
 
   Color _getColorForMood(int mood) {
@@ -160,7 +176,6 @@ void _checkUserInputForToday() async {
   }
 
   void _selectMoodAndNavigate(BuildContext context, int moodValue) async {
-    // Set the mood value using the provider
     final moodProvider = Provider.of<MoodProvider>(context, listen: false);
     moodProvider.setMoodValue(moodValue);
 
