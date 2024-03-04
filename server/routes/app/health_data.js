b086bb82aa14a4c2c0e40cc97120afc29077613c
ThomasSchiolerH <thomas@schioeler.com>
2024-03-04 15:57:05 +0100

    upload bug fixed

--- server/routes/app/health_data.js
+++ server/routes/app/health_data.js
@@ -46,11 +46,11 @@ healthRouter.post('/api/users/:userId/healthData', authenticate, async (req, res
 
     // Check if there's already an entry for the current day
     const existingData = user.healthData.find(d => 
-      d.date.toISOString().split('T')[0] === new Date(date).toISOString().split('T')[0]
+      d.date.toISOString().split('T')[0] === new Date(date).toISOString().split('T')[0] && d.type === type
     );
-
+    
     if (existingData) {
-      return res.status(409).json({ msg: 'Health data for this day already exists.' });
+      return res.status(409).json({ msg: `Health data for ${type} on this day already exists.` });
     }
 
     // Add the new health data
