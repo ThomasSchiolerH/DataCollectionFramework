710ac3279426cb10e696c760eff4af5a8c85822f
ThomasSchiolerH <thomas@schioeler.com>
2024-03-06 11:48:58 +0100

    small fix

--- App/lib/features/home/screens/analyze_screen.dart
+++ App/lib/features/home/screens/analyze_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mental_health_app/features/home/services/analyse_services.dart';
-import 'package:provider/provider.dart';
 
 class AnalyseScreen extends StatefulWidget {
   static const String routeName = '/analyse';
