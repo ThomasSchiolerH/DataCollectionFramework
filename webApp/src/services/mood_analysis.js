dc55b8babc6e450c098855fe38af0d0c5aca8103
ThomasSchiolerH <thomas@schioeler.com>
2024-03-11 13:10:51 +0100

    webapp feedback

--- webApp/src/services/mood_analysis.js
+++ webApp/src/services/mood_analysis.js
@@ -3,11 +3,31 @@ const serverURL = process.env.REACT_APP_SERVER_URL;
 
 const getToken = () => localStorage.getItem("token");
 
+// Fetching the average health data values for each type - compare it to mood
 export const fetchMoodAnalysis = async (userId) => {
   try {
     const token = getToken();
     const response = await axios.get(
       `${serverURL}/api/users/${userId}/avgHealthData`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching health data:", error);
+    throw error; // re-throw the error for handling by the caller
+  }
+};
+
+// Fetch the analysis feedback
+export const fetchMoodFeedback = async (userId) => {
+  try {
+    const token = getToken();
+    const response = await axios.get(
+      `${serverURL}/api/users/${userId}/analyseStepsMoodWeekly`,
       {
         headers: {
           Authorization: `Bearer ${token}`,
