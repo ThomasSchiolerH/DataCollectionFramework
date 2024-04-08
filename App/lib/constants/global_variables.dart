e42ef61d9845ced310be7f61e700147196add248
ThomasSchiolerH <thomas@schioeler.com>
2024-04-08 10:51:43 +0200

    colour

--- App/lib/constants/global_variables.dart
+++ App/lib/constants/global_variables.dart
@@ -3,10 +3,10 @@ String IP = "000.000.0.00"; // Enter your IP
 String uri = "http://$IP:3000";
 class GlobalVariables {
 
-  static const secondaryColor = Color.fromRGBO(132, 53, 189, 1);
+  static const secondaryColor = Color.fromARGB(255, 47, 75, 107);
   static const backgroundColor = Colors.white;
-  static const Color greyBackgroundCOlor = Color(0xffebecee);
-  static var selectedNavBarColor = Colors.cyan[800]!;
-  static const unselectedNavBarColor = Colors.black87;
+  static const Color greyBackgroundColor = Color.fromRGBO(244,244,244, 1);
+  static const selectedNavBarColor = Color.fromARGB(255, 94, 147, 207);
+  static const unselectedNavBarColor = Color.fromRGBO(32, 46, 60, 1);
 
 }
